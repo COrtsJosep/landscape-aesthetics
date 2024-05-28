8735c2e669392f83ec813d7511ab6b31ea3de0fe
Josep <corts.josep@gmail.com>
2024-05-28 14:30:13 +0000

    begin with processing of ns6, #9

---
+++ src/features/process_api_results_ns6.py
@@ -0,0 +1,42 @@
+import ast
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# IN PROGRESS
+
+#pd.set_option('display.max_columns', None)
+
+file_location_path = Path(__file__)
+project_base_path = file_location_path.parent.parent.parent
+wikimap_path = project_base_path / 'data' / 'interim' / 'wikimap_toolforge'
+
+def process_ns6_df(df_path):
+    df = pd.read_csv(df_path)
+    
+    #df.loc[:, 'resource_lat'] = df.loc[:, 'coordinates'].apply(lambda x: ast.literal_eval(x)[0]['lat']) # maybe improve
+    #df.loc[:, 'resource_lon'] = df.loc[:, 'coordinates'].apply(lambda x: ast.literal_eval(x)[0]['lon'])
+    #df.loc[:, 'are_primary_coords'] = df.loc[:, 'coordinates'].apply(lambda x: ast.literal_eval(x)[0]['primary'])
+    #df.loc[:, 'count_labels'] = df.loc[:, 'entityterms.label'].apply(lambda x: len(ast.literal_eval(x)) if type(x) == str else 0)
+    #df.loc[:, 'labels'] = df.loc[:, 'entityterms.label'].apply(lambda x: ','.join(ast.literal_eval(x)) if type(x) == str else np.nan)
+    
+    #df = df.drop(columns = ['entityterms.label', 'coordinates'])
+    #df.columns = [x.replace('.', '_') for x in df.columns.to_list()]
+    #df = df.loc[~df.loc[:, 'resource_lat'].isna()] # drop if coordinates are empty
+
+    return df
+
+ns6_dfs = []
+for country_path in wikimap_path.glob('*'):
+    ns6_dfs += [process_ns6_df(df_path) for df_path in (country_path / 'ns6').glob('*.csv')]
+
+output_path = project_base_path / 'data' / 'processed' / 'wikimap_toolforge' / 'ns6.parquet'
+output_path.parent.mkdir(parents = True, exist_ok = True)
+
+(
+    pd
+    .concat(ns6_dfs)
+    .drop_duplicates(subset = ['pageid', 'title'])
+    .reset_index()
+    .to_parquet(output_path, index = False)
+)
