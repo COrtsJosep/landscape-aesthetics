db66ad81778fbd7c64499a3c943bf94c0d040e90
Josep <corts.josep@gmail.com>
2024-06-18 14:01:00 +0000

    Documentation for the Wikimedia download process (#21)

--- src/features/wikimedia_ns6_api_caller.py
+++ src/features/wikimedia_ns6_api_caller.py
@@ -8,6 +8,9 @@ project_base_path = file_location_path.parent.parent.parent
 ns6_parquet_path = project_base_path / 'data' / 'processed' / 'wikimap_toolforge' / 'ns6.parquet'
 download_log_path = project_base_path / 'data' / 'processed' / 'wikimedia_commons' / 'temp' / 'downloaded_groups_log_ns6.pkl'
 
+# This code executes the download of the images from the Wikimedia API. The download process
+# is kept track through a list of downloaded groups, saved as a pickle file.
+
 if download_log_path.exists():
     with open(download_log_path, 'rb') as f:
         downloaded_groups = pickle.load(f)
@@ -16,7 +19,6 @@ else:
     downloaded_groups = []
     
 df_ns6 = pd.read_parquet(ns6_parquet_path, columns = ['title', 'country', 'query_id'])
-
 
 for group_name, group in df_ns6.groupby(by = ['country', 'query_id']):
     if group_name in downloaded_groups:
