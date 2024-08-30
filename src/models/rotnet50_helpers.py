6ca19a3ced1deb90912bef63ea1f2cfc28ac476b
Josep <corts.josep@gmail.com>
2024-08-30 20:05:14 +0000

    Adds trained RotNet50 that detects rotation (#35)
    Finally trained a model that successfully detects rotated images. I first train an epoch on the COCO dataset, then train 5 epochs on the handlabelled dataset, with custom weights
    
    I also cleaned and deleted code that used the Streetview dataset, since it did not give good results.

--- src/models/rotnet50_helpers.py
+++ src/models/rotnet50_helpers.py
@@ -9,7 +9,6 @@ from torchvision.transforms import v2
 
 file_location_path = Path(__file__)
 project_base_path = file_location_path.parent.parent.parent
-street_data_path = project_base_path / 'data' / 'external' / 'streetview'
 coco_data_path = project_base_path / 'data' / 'external' / 'COCO'
 
 class HandlabelledDataset(torch.utils.data.Dataset):
@@ -28,20 +27,6 @@ class HandlabelledDataset(torch.utils.data.Dataset):
         im = self.__transforms(Image.open(project_base_path / self.__impaths[idx])).float()
         t = self.__targets[idx]
         return im, t
-
-class StreetviewDataset(torch.utils.data.Dataset):
-    def __init__(self, transforms, idxs):
-        self.__transforms = transforms
-        self.__idxs = idxs
-
-    def __len__(self):
-        return len(self.__idxs)
-
-    def __getitem__(self, idx):
-        t = random.choice([0, 1, 2, 3]) # randomly rotate the image
-        im = self.__transforms(Image.open(street_data_path / f'{self.__idxs[idx]}.png')).float()
-        
-        return v2.functional.rotate(im, t * 90), t
 
 class COCODataset(torch.utils.data.Dataset):
     def __init__(self, transforms, train = True):
---
+++ src/models/rotnet50_training.py
@@ -0,0 +1,35 @@
+import torch
+import torchvision
+from pathlib import Path
+from torchvision.transforms import v2
+from rotnet50_helpers import COCODataset, train_eval
+
+file_location_path = Path(__file__)
+project_base_path = file_location_path.parent.parent.parent
+
+transforms = v2.Compose([
+    v2.PILToTensor(),
+    v2.CenterCrop(size = (600, 600)),
+    v2.Resize(size = (224, 224)),
+    v2.ToDtype(torch.float32, scale = True),
+    v2.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
+])
+
+ds_train = COCODataset(transforms = transforms, train = True)
+dl_train = torch.utils.data.DataLoader(ds_train, 32, shuffle = True)
+
+ds_test = COCODataset(transforms = transforms, train = False)
+dl_test = torch.utils.data.DataLoader(ds_test, 64, shuffle = True)
+
+network = torchvision.models.resnet50(weights = 'DEFAULT')
+network.fc = torch.nn.Linear(in_features = 512 * 4, out_features = 4)
+
+### Training
+train_eval(network = network, 
+           epochs = 1, 
+           lr = 0.001, 
+           momentum = 0.9, 
+           dl_train = dl_train, dl_test = dl_test,
+           verbose = True
+)
+torch.save(network, project_base_path / 'models' / 'rotnet50_pretrained_COCO.pth')
