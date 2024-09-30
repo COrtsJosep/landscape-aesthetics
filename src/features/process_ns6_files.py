399570e48d0c3e22b165e5ce10613d1d79f27602
Josep <corts.josep@gmail.com>
2024-09-30 13:22:30 +0000

    Code to split wiki dataset into random small chunks (#67)

---
+++ src/features/process_ns6_files.py
@@ -0,0 +1,79 @@
+import tqdm
+import math
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+pd.set_option('future.no_silent_downcasting', True)
+
+file_location_path = Path(__file__)
+project_base_path = file_location_path.parent.parent.parent
+ns6_wiki_paths = (project_base_path / 'data' / 'processed' / 'wikimedia_commons').glob('ns6_*.parquet')
+ns6_rot_paths = (project_base_path / 'data' / 'processed' / 'rotation_predicted').glob('ns6_rotation_*.parquet')
+
+well_oriented_image_paths = (
+    pd
+    .concat([pd.read_parquet(ns6_rot_path) for ns6_rot_path in ns6_rot_paths])
+    .query('rotation == 0')
+    .loc[:, 'image_path']
+)
+
+relevant_cols = [
+ 'ns6_normalized_title',
+ 'ns6_unnormalized_title',
+ 'date_time',
+ 'gps_latitude',
+ 'gps_longitude',
+ 'gps_map_datum',
+ 'license',
+ 'url',
+ 'metadata_path',
+ 'image_path',
+ #'date_time_original',
+ 'query_id',
+ 'country']
+
+def clean_df(path, relevant_cols, well_oriented_image_paths):
+    df = pd.read_parquet(path)    
+    logical_mask = (
+    	(df.loc[:, 'image_path'].apply(lambda x: 'Not Downloaded' not in x))
+    	& (df.loc[:, 'artist'].apply(lambda artist: not ('NASA' in artist or 'ESA' in artist)))
+        & (df.loc[:, 'image_path'].isin(well_oriented_image_paths))
+    )
+
+    return (
+        df
+        .replace(to_replace = 'nan', value = np.nan)
+        .dropna(subset = relevant_cols)
+        .drop_duplicates(subset = 'image_path')
+        .loc[logical_mask, 'image_path']
+)
+
+clean_image_paths = pd.Series(data = None, name = 'image_path')
+for ns6_wiki_path in tqdm.tqdm(ns6_wiki_paths, total = 8, desc = 'Generating the filtered image path list'):
+    clean_image_paths = pd.concat([clean_image_paths, clean_df(ns6_wiki_path, relevant_cols, well_oriented_image_paths)], ignore_index = True)
+
+del well_oriented_image_paths
+clean_image_paths = clean_image_paths.sample(frac = 1, random_state = 42).reset_index(drop = True) # shuffle the images
+print('Number of clean, usable images:', clean_image_paths.shape[0])
+
+def filter_df(path, image_paths):
+    df = pd.read_parquet(path)
+    return df.loc[df.loc[:, 'image_path'].isin(image_paths)]
+
+chunk_size = 75000
+num_chunks = math.ceil(clean_image_paths.shape[0] / chunk_size)
+print(f'Going to split the database into {num_chunks} chunks of {chunk_size} observations!')
+for i in tqdm.tqdm(range(num_chunks), desc = 'Creating and saving chunks'):
+    chunk_image_paths = clean_image_paths[(i * chunk_size):((i + 1) * chunk_size)]
+    ns6_wiki_paths = (project_base_path / 'data' / 'processed' / 'wikimedia_commons').glob('ns6_*.parquet')
+    output_path = project_base_path / 'data' / 'processed' / 'wikimedia_commons' / 'clean' / f'ns6_clean_{i:02}.parquet'
+    output_path.parent.mkdir(parents = True, exist_ok = True)
+
+    (
+        pd
+        .concat([filter_df(ns6_wiki_path, chunk_image_paths) for ns6_wiki_path in ns6_wiki_paths])
+        .to_parquet(output_path, index = False)
+    )
+    
+        
