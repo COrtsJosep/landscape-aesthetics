c8e9cc845a6895a00ca85241e1ced454f070151c
Josep <corts.josep@gmail.com>
2024-10-11 15:17:12 +0000

    Advances in interactive plotting (#68)

--- src/visualization/map_scenicness_europe_ep.py
+++ src/visualization/map_scenicness_europe_ep.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from tqdm.contrib import tenumerate
 from matplotlib import pyplot as plt
 from mpl_toolkits.basemap import Basemap, cm
 
@@ -32,27 +33,28 @@ df = (
     .drop('gps_map_datum', axis = 1)
 )
 
-nsteps = 500
+nsteps = 200
 
-latstepsize = (maxlat - minlat) / nsteps 
-latgrid = np.arange(minlat, maxlat + latstepsize, latstepsize)[:nsteps + 1]
+latstep = (maxlat - minlat) / nsteps 
+lonstep = (maxlon - minlon) / nsteps 
 
-lonstepsize = (maxlon - minlon) / nsteps 
-longrid = np.arange(minlon, maxlon + lonstepsize, lonstepsize)[:nsteps + 1]
+step = min(latstep, lonstep)
 
-df.loc[:, 'lat_grid'] = ((df.loc[:, 'gps_latitude'] - minlat) // latstepsize) * latstepsize + minlat
-df.loc[:, 'lon_grid'] = ((df.loc[:, 'gps_longitude'] - minlon) // lonstepsize) * lonstepsize + minlon
+latgrid = np.arange(minlat, maxlat + step, step)
+latgrid = latgrid[latgrid <= maxlat]
+lat_centers = (latgrid[:-1] + latgrid[1:]) / 2
+longrid = np.arange(minlon, maxlon + step, step)
+longrid = longrid[longrid <= maxlon]
+lon_centers = (longrid[:-1] + longrid[1:]) / 2
 
-mat = (
-    df
-    .loc[:, ['lat_grid', 'lon_grid', 'predicted_score']]
-    .rename(columns = {'predicted_score': 'scenicness'})
-    .groupby(by = ['lat_grid', 'lon_grid']).mean()
-    .reset_index()
-    .pivot(index = 'lat_grid', columns = 'lon_grid', values = 'scenicness')
-)
+mat = np.zeros((len(latgrid) - 1, len(longrid) - 1))
+for i, clat in tenumerate(lat_centers):
+    for j, clon in enumerate(lon_centers):
+        df.loc[:, 'u'] = np.sqrt((df.loc[:, 'gps_latitude'] - clat) ** 2 + (df.loc[:, 'gps_longitude'] - clon) ** 2) / 1
+        df_u = df.loc[df.loc[:, 'u'] <= 1]
+        mat[i, j] = (df_u.loc[:, 'predicted_score'] * df_u.loc[:, 'u']).sum() / df_u.loc[:, 'u'].sum() if not df_u.empty else np.nan
 
-fig = plt.figure(figsize=(12,12))
+fig = plt.figure(figsize = (12, 12))
 ax = fig.add_axes([0.1,0.1,0.8,0.8])
 
 m = Basemap(projection = 'mill', # other projections: https://matplotlib.org/basemap/stable/users/mapsetup.html
@@ -65,9 +67,9 @@ m = Basemap(projection = 'mill', # other projections: https://matplotlib.org/bas
 m.drawcoastlines()
 m.drawcountries()
 
-x, y = m(*np.meshgrid(mat.columns, mat.index))
+x, y = m(*np.meshgrid(longrid, latgrid))
 
-cs = m.contourf(x = x, y = y, data = mat, cmap = cm.GMT_seis, levels = 20)
+cs = m.pcolormesh(x = x, y = y, data = mat, cmap = cm.GMT_seis)
 cbar = m.colorbar(cs, location = 'bottom', pad = "5%")
 cbar.set_label('Average Scenicness Score')
 plt.show()
